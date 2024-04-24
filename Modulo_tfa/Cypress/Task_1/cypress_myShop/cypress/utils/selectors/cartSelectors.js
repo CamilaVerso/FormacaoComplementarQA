948d9791bc6962f0a2bc6e1bdb67c5fbf58f6c81
CamilaVerso <camilaverso2@gmail.com>
2024-04-24 16:00:54 -0300

    add mapeamento dos seletores para fazer os testes do carrinho

---
+++ Modulo_tfa/Cypress/Task_1/cypress_myShop/cypress/utils/selectors/cartSelectors.js
@@ -0,0 +1,31 @@
+export const cartSelector = {
+    selectors:{
+        homePageInfo: "#home-page-tabs > li.active > a",
+        homePageSignUpBtn: "#header > div.nav > div > div > nav > div.header_user_info > a",     
+        btnCart: "#header > div:nth-child(3) > div > div > div:nth-child(3) > div > a",
+        textYourShoppingCart: "#columns > div.breadcrumb.clearfix",
+        textCarEmpty: "#center_column > p",
+        categoryDresses: "#block_top_menu > ul > li:nth-child(2) > a",
+        selectedDress: "#center_column > ul > li:nth-child(2) > div > div.left-block > div > a.product_img_link > img",
+        selectDressSize: "#group_1 > option:nth-child(3)",
+        btnAddCart: "#add_to_cart > button",
+        btnProceedToCheckout: "#layer_cart > div.clearfix > div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > a",
+        textConfirmCart: "#cart_title > span",
+        btnProceedToCheckoutInShopCart: "#center_column > p.cart_navigation.clearfix > a.button.btn.btn-default.standard-checkout.button-medium",
+        textAddresses: "#columns > div.breadcrumb.clearfix",
+        btnProceedCheckoutInAddress: "#center_column > form > p > button > span",
+        textShipping: "#columns > div.breadcrumb.clearfix",
+        checkBoxAcceptTerms: "#cgv",
+        btnProceedToCheckoutInShipping: "#form > p > button > span",
+        btnPayment: "#HOOK_PAYMENT > div:nth-child(1) > div > p > a",
+        textBankWirePayment: "#center_column > h1",
+        textPayment: "#center_column > h1",
+        btnConfirmOrder: "#cart_navigation > button",
+        emailField: "#email",
+        passwordField: "#passwd",
+        signInBtn: "#SubmitLogin",
+        loginInfotext: "#center_column > p",     
+  
+    }
+
+}
